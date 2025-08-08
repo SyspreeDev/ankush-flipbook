39342609f6e0eb5bb85bfa75eac4ffdff9aca3db
development@syspreedigital.com <SyspreeDev>
2025-08-08 12:10:44 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -1,14 +1,29 @@
-'use client'
-import React from "react";
+'use client';
+
+import { useEffect } from 'react';
+
 const Page = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://static.elfsight.com/platform/platform.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      // Clean up if needed
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
-      {/* <!-- Elfsight Flipbook | Untitled Flipbook --> */}
-      <script src="https://static.elfsight.com/platform/platform.js" async></script>
-        <div className="elfsight-app-a6a135d9-eca0-483b-aaf7-1e900a42c412" data-elfsight-app-lazy></div>
+      {/* Elfsight Flipbook Widget */}
+      <div
+        className="elfsight-app-a6a135d9-eca0-483b-aaf7-1e900a42c412"
+        data-elfsight-app-lazy
+      ></div>
     </div>
   );
 };
 
 export default Page;
-
